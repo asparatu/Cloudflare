f79a9b389a50b4fbf8739c850f28a85e965861bc
Shane Saunders <asparatu@gmail.com>
2024-04-03 21:52:46 +0800

    Added Cloudflare R2 Configuration

--- src/Cloudflare.Console/Program.cs
+++ src/Cloudflare.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon.Runtime;
+using Amazon.S3;
+
+namespace Cloudflare.Console;
+
+class Program
+{
+    private static IAmazonS3? _s3Client;
+
+    public static void Main(string[] args)
+    {
+        var accessKey = "<ACCESS_KEY>";
+        var secretKey = "<SECRET_KEY>";
+        var credentials = new BasicAWSCredentials(accessKey, secretKey);
+        _s3Client = new AmazonS3Client(credentials, new AmazonS3Config
+            {
+                ServiceURL = "https://<ACCOUNT_ID>.r2.cloudflarestorage.com",
+            });
+    }
+}  
